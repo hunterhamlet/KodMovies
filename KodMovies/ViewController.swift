06dabb304b769968c496169fb0e39e2352a44225
Jonatan Hamlet Munguia Martinez <hunterhamlet@gmail.com>
2022-01-26 21:24:55 -0600

    Initial Commit

---
+++ KodMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KodMovies
+//
+//  Created by Hamon on 26/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
